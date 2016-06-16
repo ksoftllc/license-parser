7d8098bc2038b5616257e8a34eb621af83d11a46
Clayton LZ <clayton@hauz.io>
2016-06-15 21:26:32 -0700

    Better AAMVA Version information in README

---
+++ Example/Tests/VersionFiveParserSpec.swift
@@ -0,0 +1,9 @@
+//
+//  VersionFiveParserSpec.swift
+//  LicenseParser
+//
+//  Created by Clayton Lengel-Zigich on 6/15/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
---
+++ LicenseParser/Classes/VersionFiveParser.swift
@@ -0,0 +1,9 @@
+//
+//  VersionFiveParser.swift
+//  Pods
+//
+//  Created by Clayton Lengel-Zigich on 6/15/16.
+//
+//
+
+import Foundation
